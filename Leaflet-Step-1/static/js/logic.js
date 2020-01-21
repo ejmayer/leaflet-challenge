deecaaa15ad8b76bf3e85dd51b3841f3f19b975e
Emayer <53119577+jeneric1023@users.noreply.github.com>
2020-01-21 00:33:38 -0600

    working data

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -20,7 +20,7 @@ function createMap(earthquakes) {
 
   // Create the map object with options
   var map = L.map("map", {
-    center: [39.828175,-98.579500],
+    center: [37.7749, -122.4194],
     zoom: 4,
     layers: [lightmap, earthquakes]
   });
@@ -31,51 +31,43 @@ function createMap(earthquakes) {
   }).addTo(map);
 }
 
-
-
-
-
-
-
-
   
 function createMarkers(response) {
   
   // Pull the event data
-  var data = response.features;
+  var earthquakes = response.features;
+  console.log(earthquakes)
 
-  console.log(data.geometry);
 
+  // create markers
   // Initialize an array to hold quake markers
   var quakeMarkers = [];
 
-  // Loop through the earthquakes array
-  for (var index = 0; index < data.length; index++) {
-    var quakeList = data[index];
+  for (var i = 0; i < earthquakes.length; i++) {
+    var earthquake = earthquakes[i];
+    var magnitude = earthquake.properties.mag; 
+
+    console.log(magnitude);
+
+    // Adding circular markers + popups 
+    quakeMarker = L.circle([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]], {
+      fillOpacity: 0.75,
+      color: "white",
+      fillcolor: "white",
+      // fillColor: circleColor(+magnitude),
+      radius: magnitude * 500
+    });
     
-
-    // var quakePlaces = [];
-    // var quakePlace = data.properties.place;
-    // quakePlaces.push(quakePlace);
-
-    // console.log(quakePlace);
-
-
-    console.log(quakeList);
-
-
-    // For each quake, create a marker and bind a popup with the quake's location and magnitude
-    var quakeMarker = L.marker([quakeList.geometry.coordinates[0],quakeList.geometry.coordinates[1]])
-      .bindPopup("<h3>Location:" + quakeList.properties.place + "<h3><h3>Magnitude: " + quakeList.properties.mag + "<h3>");
+    // .bindPopup("<h1>" + earthquake.properties.place + "</h1> <hr> <h3>Magnitude: " + magnitude + "</h3>").addTo(map).addTo(map); 
 
     // Add the marker to the quakeMarkers array
     quakeMarkers.push(quakeMarker);
 
-    console.log("quakemarkers:"+quakeMarker);
-  }
+    console.log(quakeMarkers);
+  };
 
-// Create a layer group made from the quake markers array, pass it into the createMap function
-createMap(L.layerGroup(quakeMarkers));
+  // Create a layer group made from the quake markers array, pass it into the createMap function
+  createMap(L.layerGroup(quakeMarkers));
   
 }
 
